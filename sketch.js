40ecc13d662cddf2f218b0593f3839f07628c282
bhyejin <bhyejin@karts.ac.kr>
2021-06-12 16:23:28 +0900

    1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,48 @@
+let snake;
+let rez = 10;
+let food;
+let w;
+let h;
+
 function setup() {
+    createCanvas(400, 400);
+    w = floor(width / rez);
+    h = floor(height / rez);
+    frameRate(5); 
+    snake = new Snake();
+    food = createVector();
+    foodLocation();
+
+}
+
+function foodLocation() {
+
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        snake.setDir(-1, 0);
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.setDir(1, 0);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.setDir(0, 1);
+    } else if (keyCode === UP_ARROW) {
+        snake.setDir(0, -1);
+    }
 
 }
 
 function draw() {
-  
+    scale(rez);
+    background(220);
+    if (snake.eat(food)) {
+        foodLocation();
+    }
+    snake.update();
+    snake.show();
+    snake.eat(food);
+
+    noStroke();
+    fill(255, 0, 0);
+    rect(food.x, food.y, 1, 1);
 }
